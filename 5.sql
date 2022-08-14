9249fe8b39e18881632b9b4901d269018b599580
Sergey Vatagin <s.vatagin@gmail.com>
2022-08-14 20:59:15 +0300

    Done 5th question

--- 5.sql
+++ 5.sql
@@ -1,16 +1,14 @@
-WITH max_count AS (
-SELECT t.data, MAX(t.data_count) AS maxc FROM
-(SELECT
+WITH count_t AS (
+SELECT
 d.sign_date data,
 count(d.id) data_count
 FROM documents d
 GROUP BY d.sign_date
-) t
-GROUP BY t.data
 )
-SELECT * from max_count t1
-JOIN max_count t2
+SELECT 'Максимум документов: дата: ' || t1.data || ' , кол-во: ' || t1.data_count
+FROM count_t t1
+JOIN
+(SELECT MAX(count_t.data_count) maxc FROM count_t) t2
 ON 
-t1.data = t2.data
+t1.data_count = t2.maxc;
 
-;
