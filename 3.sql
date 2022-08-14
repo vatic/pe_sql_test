4c5bcbb7bdc109d03b5acb79cd12073332e04be4
Sergey Vatagin <s.vatagin@gmail.com>
2022-08-14 15:19:44 +0300

    Done 3rd question

--- 3.sql
+++ 3.sql
@@ -1,13 +1,12 @@
 SELECT
 dt.name "Тип документа",
-contract_number "№ документа",
-sign_date "Дата",
-0 AS "Сумма по документу",
-contract_id,
-cp1.name "Поставщик",
-cp2.name "Плательщик"
-FROM contracts c
--- INNER JOIN document_types dt ON c.document_type_id = dt.id
-INNER JOIN counterparties cp1 ON c.provider_id = cp1.id
-INNER JOIN counterparties cp2 ON c.payer_id = cp2.id
+d.document_number "№ документа",
+d.sign_date "Дата",
+SUM(i.price) AS "Сумма по документу",
+(SELECT cp1.name FROM contracts c INNER JOIN counterparties cp1 ON c.provider_id = cp1.id WHERE d.contract_id = c.id) AS "Поставщик",
+(SELECT cp2.name FROM contracts c INNER JOIN counterparties cp2 ON c.payer_id = cp2.id WHERE d.contract_id = c.id) AS "Плательщик"
+FROM documents d
+INNER JOIN document_types dt ON d.document_type_id = dt.id
+LEFT OUTER JOIN items i ON d.id = i.document_id
+GROUP BY d.id, dt.name, d.document_number, d.sign_date
 ;
